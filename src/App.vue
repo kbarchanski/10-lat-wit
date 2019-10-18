7efe4902c4354948bf9e8e7a1726e29abc4cbf49
kbarchanski <kbarchanski@gmail.com>
2019-10-19 00:07:40 +0200

    szukanie z podpowiedzia

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="container" id="app">
     <h1>Przygarnij psa</h1>
-    <div class="form-group">
+    <div class="form-group position-relative">
       <label for="filter">Filtruj</label>
-      <input class="form-control" id="filter" v-model="value" />
+      <input class="form-control" id="filter" autocomplete="off" v-model="value" />
+      <div v-if="typeahead.length" class="dropdown-menu" :class="{'show': showDropdown}">
+        <a
+          v-for="dog of typeahead"
+          :key="dog"
+          class="dropdown-item"
+          href="#"
+          @click="selectItem(dog)"
+        >
+          {{ dog }}
+        </a>
+      </div>
     </div>
     <button class="btn btn-primary" @click="search">Szukaj</button>
 
@@ -21,7 +32,7 @@
           </figcaption>
         </figure>
       </template>
-      <b-alert show dismissible variant="warning" @dismissed="reset()">
+      <b-alert v-else show dismissible variant="warning" @dismissed="reset()">
         Przykro nam, nie znaleźliśmy psa, którego szukasz
       </b-alert>
     </div>
@@ -38,7 +49,24 @@ export default {
       value: '',
       dogs: ['Abadon', 'Abar', 'Abi', 'Abis', 'Abrams', 'Abraxas', 'Abu', 'Babi', 'Baca', 'Bachus', 'Baddy', 'Badyl', 'Dalia', 'Dalima'],
       filteredDogs: [],
+      showDropdown: false,
     }
+  },
+
+  computed: {
+    typeahead () {
+      if (this.value) {
+        const regex = new RegExp(this.value, 'i')
+        return this.dogs.filter(dog => regex.test(dog))
+      }
+      return []
+    },
+  },
+
+  watch : {
+    value () {
+      this.showDropdown = true
+    },
   },
 
   created () {
@@ -60,10 +88,18 @@ export default {
       this.$set(this, 'filteredDogs', [...this.dogs])
       // mozna tez wyczyscic input
       // this.value = ''
-    }
+    },
+    selectItem (dog) {
+      this.value = dog
+      this.showDropdown = false
+      this.search()
+    },
   }
 }
 </script>
 
 <style>
+  .dropdown-menu {
+    top: 100%;
+  }
 </style>
