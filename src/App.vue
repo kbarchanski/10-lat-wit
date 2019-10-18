0e73aee77d6c1d4a13c22e8285abaa177690eaad
Krystian Barchanski <krystian@appjobs.com>
2019-10-18 22:47:17 +0200

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <div id="app">
-    <input
-      v-on:input="value = $event.target.value"
-      v-bind:value="value"
-    >
+  <div class="container" id="app">
+    <div class="form-group">
+      <label for="filter">Filtruj</label>
+      <input class="form-control" id="filter" v-model="value" />
+    </div>
+
+    <h2>Nasze pieski</h2>
+    <div class="dogs">
+      <figure v-for="(dog, index) in dogs" :key="index" class="figure">
+        <img
+          :src="`https://loremflickr.com/240/240/dog?lock=${index + 1}`"
+          class="figure-img img-fluid rounded ml-1"
+          :alt="dog"
+        />
+        <figcaption class="figure-caption">
+          {{ dog }}
+        </figcaption>
+      </figure>
+    </div>
   </div>
 </template>
 
@@ -14,19 +28,20 @@ export default {
 
   data () {
     return {
-      value: 'wartość początkowa'
+      value: 'wartość początkowa',
+      dogs: ['Abadon', 'Abar', 'Abi', 'Abis', 'Abrams', 'Abraxas', 'Abu', 'Babi', 'Baca', 'Bachus', 'Baddy', 'Badyl', 'Dalia', 'Dalima'],
     }
   },
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
