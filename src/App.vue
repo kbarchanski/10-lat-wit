ac2a9a983ec45b6289a837707e0084c18582ac2d
kbarchanski <kbarchanski@gmail.com>
2019-10-18 23:35:39 +0200

    proste wyszukiwanie

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,25 @@
       <label for="filter">Filtruj</label>
       <input class="form-control" id="filter" v-model="value" />
     </div>
+    <button class="btn btn-primary" @click="search">Szukaj</button>
 
     <h2>Nasze pieski</h2>
     <div class="dogs">
-      <figure v-for="(dog, index) in dogs" :key="index" class="figure">
-        <img
-          :src="`https://loremflickr.com/240/240/dog?lock=${index + 1}`"
-          class="figure-img img-fluid rounded ml-1"
-          :alt="dog"
-        />
-        <figcaption class="figure-caption">
-          {{ dog }}
-        </figcaption>
-      </figure>
+      <template v-if="filteredDogs.length">
+        <figure v-for="(dog, index) in filteredDogs" :key="index" class="figure">
+          <img
+            :src="`https://loremflickr.com/240/240/dog?lock=${index + 1}`"
+            class="figure-img img-fluid rounded ml-1"
+            :alt="dog"
+          />
+          <figcaption class="figure-caption">
+            {{ dog }}
+          </figcaption>
+        </figure>
+      </template>
+      <b-alert show dismissible variant="warning" @dismissed="reset()">
+        Przykro nam, nie znaleźliśmy psa, którego szukasz
+      </b-alert>
     </div>
   </div>
 </template>
@@ -29,10 +35,33 @@ export default {
 
   data () {
     return {
-      value: 'wartość początkowa',
+      value: '',
       dogs: ['Abadon', 'Abar', 'Abi', 'Abis', 'Abrams', 'Abraxas', 'Abu', 'Babi', 'Baca', 'Bachus', 'Baddy', 'Badyl', 'Dalia', 'Dalima'],
+      filteredDogs: [],
     }
   },
+
+  created () {
+    // prawie to samo co
+    // this.filteredDogs = [...this.dogs]
+    // z ta roznica, ze this.$set tworzy reaktywna wartosc filtered dogs
+    this.$set(this, 'filteredDogs', [...this.dogs])
+    
+    // alternatywna metoda, ale ryzykowna, bo moze spowodowac duplikacje danych w tablicy
+    // this.filteredDogs.push(...this.dogs)
+  },
+
+  methods: {
+    search () {
+      const regex = new RegExp(this.value, 'i')
+      this.$set(this, 'filteredDogs', this.dogs.filter(dog => regex.test(dog)))
+    },
+    reset () {
+      this.$set(this, 'filteredDogs', [...this.dogs])
+      // mozna tez wyczyscic input
+      // this.value = ''
+    }
+  }
 }
 </script>
 
